f69b2c1fa0230f184f5a632cdb2b25375d15c379
Michal Bakshi <michalb5980@gmail.com>
2026-02-22 17:06:57 +0200

    Remove log

--- frontend/my-app/src/axios/gameAxios.jsx
+++ frontend/my-app/src/axios/gameAxios.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 const url=process.env.REACT_APP_API_URL+"/game"
-console.log("API URL:", process.env.REACT_APP_API_URL)
 
 export const addGame=(obj)=>{
     return axios.post(`${url}/add`,obj)
